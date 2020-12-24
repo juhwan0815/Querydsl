22e2bc09ab5b6c836313b485f551460c2ac53a1d
juhwan0815 <juwom0831@naver.com>
2020-12-24 19:06:19 +0900

    QueryDSL 스터디

---
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -0,0 +1,22 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
+
+@SpringBootApplication
+public class QuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslApplication.class, args);
+	}
+
+	//	JPAQueryFactory 스프링 빈 등록
+	@Bean
+	public JPAQueryFactory queryFactory(EntityManager em){
+		return new JPAQueryFactory(em);
+	}
+}
